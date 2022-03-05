c60f1bc1b9f3f0b5cfafa56c39043c92e4443e8f
Laurence Tuchin <laurencetuchin@gmail.com>
2022-03-05 18:24:00 +1100

    split client and server

---
+++ client/src/components/ChatHeader.jsx
@@ -0,0 +1,15 @@
+const ChatHeader = () => {
+    return (
+        <div className="chat-container-header">
+        <div className="profile">
+            <div className="img-container">
+                <img src="" alt="" />
+            </div>
+            <h3 className="">Username</h3>
+        </div>
+        <i className="log-out-icon"><img src="https://img.icons8.com/metro/26/000000/exit.png"/></i>
+        </div>
+    );
+}
+
+export default ChatHeader;
